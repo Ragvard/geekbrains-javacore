1d4a453659907777ae8832ec3335abd649c4bf7e
adnesterov <adnesterov@edu.hse.ru>
2020-11-05 23:49:32 +0300

    hw done

---
+++ src/main/java/geekbrains/hw10/Server.java
@@ -0,0 +1,28 @@
+package geekbrains.hw10;
+
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+    public static void main(String[] args) {
+        try (ServerSocket serverSocket = new ServerSocket(8080)) {
+            System.out.println("Сервер запущен");
+            Socket socket = serverSocket.accept();
+            System.out.println("Клиент подключился");
+
+            try (DataInputStream inputStream = new DataInputStream(socket.getInputStream())) {
+                while (true) {
+                    String str = inputStream.readUTF();
+                    if (str.isEmpty()) break;
+                    System.out.println(str);
+                }
+            }
+
+            System.out.println("Сервер выключен");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
