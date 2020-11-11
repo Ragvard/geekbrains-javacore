bef44c2a2c18e8be463b466a20c05d77e43911f6
Ragvard <anesterov944@gmail.com>
2020-11-12 01:33:10 +0300

    hw11 done

--- src/main/java/geekbrains/hw11/Repository.java
+++ src/main/java/geekbrains/hw11/Repository.java
@@ -1,5 +1,6 @@
 package geekbrains.hw11;
 
+import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.sql.*;
@@ -21,9 +22,17 @@ public class Repository<T> {
 
     private final List<ColumnInfo> columns;
 
+    private final PreparedStatement findAllStatement;
+
     private final PreparedStatement selectByIdStatement;
 
     private final PreparedStatement insertStatement;
+
+    private final PreparedStatement deleteStatement;
+
+    private final PreparedStatement deleteAllStatement;
+
+    private final PreparedStatement updateStatement;
 
     public Repository(Class<T> repositoryClass, Connection connection) throws SQLException {
         this.repositoryClass = repositoryClass;
@@ -35,32 +44,48 @@ public class Repository<T> {
         }
         this.tableName = dbTable.tableName();
 
+        DbId dbId = null;
         this.columns = new ArrayList<>();
         for (Field field : repositoryClass.getDeclaredFields()) {
             field.setAccessible(true);
-            // TODO если поле с аннотацией @DbId не найдено, то выдать сообщение об ошибке
-            // TODO проверить, чтобы поле с аннотацией @DbId было только одно. Если их больше, то выдать сообщение об ошибке
-            DbId dbId = field.getAnnotation(DbId.class);
-            if (dbId != null) {
-                ColumnInfo columnInfo = new ColumnInfo(field.getName(), field.getName(), field.getType(), true);
-                columns.add(columnInfo);
-                continue;
-            }
-            DbColumn dbColumn = field.getAnnotation(DbColumn.class);
-            if (dbColumn != null) {
-                ColumnInfo columnInfo = new ColumnInfo(field.getName(), dbColumn.columnName(), field.getType(), false);
-                columns.add(columnInfo);
+            int type = field.getAnnotation(DbId.class) != null ? 0 : field.getAnnotation(DbColumn.class) != null ? 1 : 2;
+            switch (type) {
+                case 0: {
+                    if (dbId != null) throw new IllegalStateException("More then one field with @DbId in class " + repositoryClass.getSimpleName());
+                    dbId = field.getAnnotation(DbId.class);
+                    ColumnInfo columnInfo = new ColumnInfo(field.getName(), field.getName(), field.getType(), true);
+                    columns.add(columnInfo);
+                    break;
+                }
+
+                case 1: {
+                    DbColumn dbColumn = field.getAnnotation(DbColumn.class);
+                    ColumnInfo columnInfo = new ColumnInfo(field.getName(), dbColumn.columnName(), field.getType(), false);
+                    columns.add(columnInfo);
+                    break;
+                }
+
+                case 2:
+                    throw new IllegalStateException("More then one field with @DbId in class " + repositoryClass.getSimpleName());
             }
         }
         createTableIfNotExists();
 
+        this.findAllStatement = buildFindAllStatement();
         this.selectByIdStatement = buildSelectByIdStatement();
         this.insertStatement = buildInsertStatement();
+        this.deleteStatement = buildDeleteStatement();
+        this.deleteAllStatement = buildDeleteAllStatement();
+        this.updateStatement = buildUpdateStatement();
     }
 
-    public List<T> findAll() {
-        // TODO
-        return Collections.emptyList();
+    public List<T> findAll() throws SQLException {
+        List<T> result = new ArrayList<>();
+        ResultSet resultSet = findAllStatement.executeQuery();
+        while (resultSet.next()) {
+            result.add(createEntityByResultSet(resultSet));
+        }
+        return result;
     }
 
     public T findById(Long id) throws SQLException {
@@ -83,12 +108,22 @@ public class Repository<T> {
         insertStatement.execute();
     }
 
-    public void update(T entity) {
-        // TODO
+    public void update(T entity) throws SQLException {
+        for (int i = 1; i < columns.size(); i++) {
+            updateStatement.setObject(i, invokeGetter(columns.get(i).fieldName, entity));
+        }
+        updateStatement.setObject(3, invokeGetter(columns.get(0).fieldName, entity));
+        updateStatement.execute();
+
     }
 
-    public void delete(Long id) {
-        // TODO
+    public void delete(Long id) throws SQLException {
+        deleteStatement.setLong(1, id);
+        deleteStatement.execute();
+    }
+
+    public void deleteAll() throws SQLException {
+        deleteAllStatement.execute();
     }
 
     private T createEntityByResultSet(ResultSet rs) throws SQLException {
@@ -158,6 +193,17 @@ public class Repository<T> {
         }
     }
 
+    private PreparedStatement buildFindAllStatement() throws SQLException {
+        StringBuilder query = new StringBuilder();
+        query.append("select" + " ");
+        query.append("*" + " ");
+        query.append("from" + " ");
+        query.append(tableName);
+        query.append(";");
+        System.out.println("Find All statement: " + query);
+        return connection.prepareStatement(query.toString());
+    }
+
     private PreparedStatement buildSelectByIdStatement() throws SQLException {
         StringBuilder query = new StringBuilder();
         query.append("select ");
@@ -193,6 +239,54 @@ public class Repository<T> {
         query.deleteCharAt(query.length() - 1);
         query.append(");");
         System.out.println("Insert statement: " + query);
+        return connection.prepareStatement(query.toString());
+    }
+
+    private PreparedStatement buildDeleteStatement() throws SQLException {
+        StringBuilder query = new StringBuilder();
+        query.append("delete" + " ");
+        query.append("from" + " ");
+        query.append(tableName).append(" ");
+        query.append("where" + " ");
+        query.append("id =" + " ");
+        query.append("?");
+        query.append(";");
+        System.out.println("Delete statement: " + query);
+        return connection.prepareStatement(query.toString());
+    }
+
+    private PreparedStatement buildDeleteAllStatement() throws SQLException {
+        StringBuilder query = new StringBuilder();
+        query.append("delete" + " ");
+        query.append("from" + " ");
+        query.append(tableName);
+        query.append(";");
+        System.out.println("Delete All statement: " + query);
+        return connection.prepareStatement(query.toString());
+    }
+
+    private PreparedStatement buildUpdateStatement() throws SQLException {
+        StringBuilder query = new StringBuilder();
+        query.append("update" + " ");
+        query.append(tableName).append(" ");
+        query.append("set" + " ");
+        for (ColumnInfo columnInfo : columns) {
+            if (!columnInfo.isId) {
+                query.append(columnInfo.columnName).append(" ");
+                query.append("=" + " ");
+                query.append("?");
+                query.append("," + " ");
+            }
+        }
+        query.deleteCharAt(query.length() - 1);
+        query.deleteCharAt(query.length() - 1);
+        query.append(" ");
+        query.append("where" + " ");
+        query.append("id =" + " ");
+        query.append("?");
+        query.append(";");
+
+        System.out.println("Update statement: " + query);
         return connection.prepareStatement(query.toString());
     }
 
--- src/main/java/geekbrains/hw11/TestRepository.java
+++ src/main/java/geekbrains/hw11/TestRepository.java
@@ -4,24 +4,85 @@ package geekbrains.hw11;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.List;
 
 public class TestRepository {
 
-    public static void main(String[] args) throws SQLException {
-        Connection connection = DriverManager.getConnection(
+    public static void print(Repository<User> userRepository) throws SQLException {
+        List<User> all = userRepository.findAll();
+        if (all.isEmpty()) {
+            System.out.println("Table is empty.");
+            return;
+        }
+        for (User user : all) {
+            System.out.println(user.getId() + " - " + user.getUsername() + ":" + user.getPassword());
+        }
+    }
+
+    public static void print(User user){
+        if (user == null) {
+            System.out.println("No such user.");
+            return;
+        }
+        System.out.println(user.getId() + " - " + user.getUsername() + ":" + user.getPassword());
+    }
+
+    public static void insert(Repository<User> userRepository, String name, String password) throws SQLException {
+        userRepository.insert(new User(null, name, password));
+    }
+
+    public static void delete(Repository<User> userRepository, long ID) throws SQLException {
+        userRepository.delete(ID);
+    }
+
+    public static void clear(Repository<User> userRepository) throws SQLException {
+        userRepository.deleteAll();
+    }
+
+    public static void update(Repository<User> userRepository, String name, String password, long ID) throws SQLException {
+        if (userRepository.findById(ID) == null) {
+            System.out.println("No such user to update");
+            return;
+        }
+        userRepository.update(new User(ID, name, password));
+    }
+
+    public static long getId(Repository<User> userRepository, int pos) throws SQLException {
+        return userRepository.findAll().get(pos).getId();
+    }
+
+    public static void main(String[] args){
+        try (Connection connection = DriverManager.getConnection(
                 "jdbc:mysql://localhost:3306/vtb_jdbc_lesson?createDatabaseIfNotExist=true&allowPublicKeyRetrieval=true&useSSL=false&useUnicode=true&characterEncoding=UTF-8&serverTimezone=UTC",
-                "root", "root");
+                "admin", "1234")) {
+            Repository<User> userRepository = new Repository<>(User.class, connection);
 
-        Repository<User> userRepository = new Repository<>(User.class, connection);
+            clear(userRepository);
 
-        User user = userRepository.findById(1L);
-        System.out.println(user);
+            insert(userRepository,"Ivan", "1234");
+            insert(userRepository,"Dmitry", "qwerty");
+            insert(userRepository,"Grisha", "nfv7$fa~GA9KF4bvd");
+            insert(userRepository,"Alexander", "NULL");
+            insert(userRepository,"Alexey", "password");
 
-        user = userRepository.findById(2L);
-        System.out.println(user);
+            print(userRepository);
 
-        userRepository.insert(new User(null, "ccc", "ccc"));
-        userRepository.insert(new User(null, "ddd", "ddd"));
+            System.out.println("----------------------------------------------------");
+
+            print(userRepository.findById(getId(userRepository, 3)));
+
+            System.out.println("----------------------------------------------------");
+
+            delete(userRepository, getId(userRepository, 0));
+            delete(userRepository, getId(userRepository, 0));
+
+            update(userRepository, "Alexey", "new_password", getId(userRepository, 2));
+
+            print(userRepository);
+        } catch (SQLException ex) {
+            System.out.println("Something went wrong!");
+            ex.printStackTrace();
+        }
     }
 
 }
